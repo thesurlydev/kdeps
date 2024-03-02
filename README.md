8c6cd12a1346d5baba370bad930e6ee6d8f744ce
Shane Witbeck <switbeck@mediafly.com>
2024-03-02 14:47:29 -0800

    feat: Add support for deps file and custom output dir - Add documentation to README.md

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
-# deps
+# kdeps
+
+A small library to download dependencies from a file.
+
+This project also serves as an example for using `jdeps`, `jlink`, and `jpackage` to create a custom runtime image and
+package it as a platform-specific installer.
+
+## Requirements
+
+- JDK 21
+- Kotlin
+
+## Build
+
+```shell
+./build.sh
+```
+
+## Install OS Package
+
+Note: Currently, only `deb` package is supported.
+
+```shell
+./install.sh
+```
+
+By default, kdeps gets installed in `/opt/kdeps` directory so you may need to add it to your `PATH` environment
+variable.
 
 
-jdeps --multi-release 21 --ignore-missing-deps kotlin-stdlib-jdk8-1.9.22.jar
+## Usage
+
+### Read dependencies from stdin
+
+```shell
+echo "org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.8.0" | kdeps
+```
+will download the dependency and save it to the default output directory `./lib`.
+
+### Read dependencies from a file
+
+As an example, create a file `dependencies.txt` with the following content:
+
+```text
+org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.8.0
+org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3
+```
+
+```shell
+kdeps -f dependencies.txt
+```
+
+### Define a custom output directory
+
+```shell
+kdeps -f dependencies.txt -o /path/to/output
+```
--- src/kdeps/Main.kt
+++ src/kdeps/Main.kt
@@ -1,26 +1,57 @@
 package kdeps
 
+import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.URI
 import java.nio.file.Files
 import java.nio.file.Paths
+import kotlin.system.exitProcess
 
 const val MAVEN_BASE_URL = "https://repo1.maven.org/maven2"
 const val DEFAULT_LIB_DIR = "lib"
 
 fun main(args: Array<String>) {
-    val directoryPath = args.getOrNull(0) ?: DEFAULT_LIB_DIR
-    val reader = System.`in`.bufferedReader()
-    reader.forEachLine { line ->
-        val url = line.toMavenUrl()
-        downloadJar(url, directoryPath)
+
+    // Parse "-f" for input file
+    val fileIndex = args.indexOf("-f")
+    val filePath = if (fileIndex != -1 && args.size > fileIndex + 1) args[fileIndex + 1] else null
+
+    // Parse "-o" for output directory
+    val dirIndex = args.indexOf("-o")
+    val outputDirPath = if (dirIndex != -1 && args.size > dirIndex + 1) args[dirIndex + 1] else DEFAULT_LIB_DIR
+
+    // Ensure the output directory exists
+    val outputDir = File(outputDirPath)
+    if (!outputDir.exists()) {
+        outputDir.mkdirs()
     }
-    /*Files.readAllLines(java.nio.file.Paths.get(DEPS_FILE))
-        .stream()
-        .map(String::toMavenUrl)
-        .forEach(::downloadJar)*/
+
+    if (filePath != null) {
+        // Process input from file
+        val file = File(filePath)
+        if (file.exists() && file.canRead()) {
+            file.forEachLine { line ->
+                processLine(line, outputDir) // Process and output to specified directory
+            }
+        } else {
+            System.err.println("File does not exist or cannot be read: $filePath")
+            exitProcess(1)
+        }
+    } else {
+        // Process input from System.in
+        System.`in`.bufferedReader().forEachLine { line ->
+            processLine(line, outputDir) // Process and output to specified directory
+        }
+    }
+
+    exitProcess(0)
+}
+
+fun processLine(line: String, outputDir: File) {
+    val url = line.toMavenUrl()
+    downloadJar(url, outputDir.path)
 }
 
 // transform 'org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.8.0' to 'https://repo1.maven.org/maven2/org/jetbrains/kotlinx/kotlinx-coroutines-core-jvm/1.8.0/kotlinx-coroutines-core-jvm-1.8.0.jar'
